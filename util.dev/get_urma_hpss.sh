d77dec0d70324a97c1ded97863edc94eebb0303e
Ying Lin <ying.lin@noaa.gov>
2019-11-26 18:08:16 +0000

    update

--- util.dev/get_urma_hpss.sh
+++ util.dev/get_urma_hpss.sh
@@ -2,8 +2,8 @@
 set -x
 
 #day=$1
-day1=20190202
-day2=20190203
+day1=20190913
+day2=20190929
 
 # theia:
 # finddate=/scratch4/NCEPDEV/rstprod/nwprod/util/ush/finddate.sh
@@ -11,18 +11,20 @@ day2=20190203
 UTILROOT=/gpfs/dell1/nco/ops/nwprod/prod_util.v1.1.2
 FINDDATE=$UTILROOT/ush/finddate.sh
 
-day=$day1
+day=$day2
 
-wrkdir=/gpfs/dell2/ptmp/Ying.Lin
+wrkdir=/gpfs/dell2/ptmp/Ying.Lin/prod_urma
 
-while [ $day -le $day2 ];
+while [ $day -ge $day1 ];
 do 
   mkdir -p $wrkdir/pcpurma.$day
   cd $wrkdir/pcpurma.$day
   yyyy=`echo $day | cut -c 1-4`
   yyyymm=`echo $day | cut -c 1-6`
-  htar xvf /NCEPPROD/hpssprod/runhistory/rh${yyyy}/$yyyymm/$day/com2_urma_prod_pcpurma.$day.tar
-  day=`$FINDDATE $day d+1`
+#  htar xvf /NCEPPROD/hpssprod/runhistory/rh${yyyy}/$yyyymm/$day/com2_urma_prod_pcpurma.$day.tar
+  htar xvf /NCEPPROD/hpssprod/runhistory/rh${yyyy}/$yyyymm/$day/gpfs_dell2_nco_ops_com_urma_prod_pcpurma.$day.tar
+
+  day=`$FINDDATE $day d-1`
 done
 
 exit
